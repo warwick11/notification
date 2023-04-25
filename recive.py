2a1d6435932f6ce07d32c9ecf84f409b0b5490f9
warwick11 <warwick0820@gmail.com>
2023-04-25 16:35:49 +0900

    IPアドレスを資料と連動

--- recive.py
+++ recive.py
@@ -7,15 +7,11 @@ class class1(BaseHTTPRequestHandler):
         content_len = int(self.headers.get('Content-Length'))
         post_body = self.rfile.read(content_len)
         
-
-        #jsonをパースして連想配列にする
-        post_dir = json.loads(post_body)
-        post_doc = str(post_dir)
-        #受信データを表示
-        print(post_dir)
+        #受信したデータを表示
+        print(post_body)
 
 #PCのIPアドレス
-ip = '192.168.3.10'
+ip = '192.168.3.60'
 
 #使用するポート番号
 port = 8000
@@ -25,6 +21,7 @@ server = HTTPServer((ip, port), class1)
 
 try:
     while True:
+        #サーバーを実行
         server.serve_forever()
 
 except KeyboardInterrupt:
